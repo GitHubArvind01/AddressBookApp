72f37ea2fe7df18237481af18e642ba59dc4bc86
GitHubArvind01 <kumararvind21390@gmail.com>
2026-03-07 16:43:40 +0530

    [ARVIND] : Added the program of UC18

--- src/main/java/com/addressbook/addressbookapp/repository/ContactRepository.java
+++ src/main/java/com/addressbook/addressbookapp/repository/ContactRepository.java
@@ -1,5 +1,7 @@
 package com.addressbook.addressbookapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.addressbook.addressbookapp.model.Contact;
 public interface ContactRepository extends JpaRepository<Contact, Long>{
 	boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+    
+    List<Contact> findByCityIgnoreCase(String city);
 }
